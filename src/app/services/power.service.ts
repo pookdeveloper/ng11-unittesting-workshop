7ca0489e9b0c8c7c72fd4272e57ea99195ba31c2
miquelnez <miquelnez@gmail.com>
2021-02-26 10:20:56 +0100

    service doc

--- src/app/services/power.service.ts
+++ src/app/services/power.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * As an architect, I want a new function with two parameters, one for the base and other for the exponent.
+ * The result should be a number.
+ * example: 2 as base, 2 as exponent, should be 4
+ */
 export class PowerService {
 
   constructor() { }
